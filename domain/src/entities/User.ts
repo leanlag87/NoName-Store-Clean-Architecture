2d887432e43013fbeb2399626e90fd0368aa0a58
Leandro Gonzalez <leanlag@gmail.com>
2025-07-23 20:00:29 -0300

    Agregar la interfaz User con propiedades esenciales y un tipo UserRole para gestionar roles de usuario en el sistema.

--- domain/src/entities/User.ts
+++ domain/src/entities/User.ts
@@ -1,1 +1,11 @@
-console.log("Hola Coders!");
+type UserRole = "admin" | "user";
+
+export interface User {
+    id: string;
+    username: string;
+    email: string;
+    password: string;
+    role: UserRole;
+    createdAt: Date;
+    updatedAt: Date;
+}

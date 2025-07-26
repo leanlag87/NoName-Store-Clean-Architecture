7766f6adab0626de7dda438fb52bed73f791ea3e
Leandro Gonzalez <leanlag@gmail.com>
2025-07-26 15:29:30 -0300

    Agregar la interfaz UserRepository para gestionar operaciones de usuario en el sistema.

---
+++ domain/src/repositories/user-repository.ts
@@ -0,0 +1,10 @@
+import { User } from "../entities/User";
+
+export interface UserRepository {
+    findByEmail(email: string): Promise<User | null>;
+    save(user: User): Promise<void>;
+    findById(id: string): Promise<User | null>;
+    create(user: User): Promise<User>;
+    update(user: User): Promise<User>;
+    delete(id: string): Promise<void>;
+}

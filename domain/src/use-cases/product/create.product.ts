a05f85e628621221efdcc470b09b7eceb397f956
Leandro Gonzalez <leanlag@gmail.com>
2025-07-28 15:42:50 -0300

    feat(domain): implementar validación de datos al crear productos

--- domain/src/use-cases/product/create.product.ts
+++ domain/src/use-cases/product/create.product.ts
@@ -1,6 +1,6 @@
 import { Product } from "../../entities/Product";
 import { ProductRepository } from "../../repositories/product-repository";
-import { InvalidDataError } from "../../errors/error";
+import { createInvalidDataError, InvalidDataError } from "../../errors/error";
 
 export type CreateProductRequestModel = Omit<
   Product,
@@ -12,8 +12,45 @@ export interface CreateProductDependencies {
 }
 
 export async function createProduct(
-  _dependencies: CreateProductDependencies,
-  _payload: CreateProductRequestModel
+  { productRepository }: CreateProductDependencies,
+  { name, description, price, categoryId, brandId }: CreateProductRequestModel
 ): Promise<InvalidDataError | Product> {
-  return null as any;
+  const hasErrors = validateProductData(name, description, price);
+  if (hasErrors) return hasErrors;
+
+  const productData: Omit<Product, "id"> = {
+    name,
+    description,
+    price,
+    categoryId,
+    brandId,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  return await productRepository.create(productData);
+}
+
+function validateProductData(
+  name: string,
+  description: string,
+  price: number
+): InvalidDataError | void {
+  if (name.trim() === "") {
+    return createInvalidDataError("Name must be not empty");
+  }
+
+  if (name.length > 20) {
+    return createInvalidDataError("Name cannot be longer than 20 characters");
+  }
+
+  if (description.length > 500) {
+    return createInvalidDataError(
+      "Description cannot be longer than 500 characters"
+    );
+  }
+
+  if (price <= 0) {
+    return createInvalidDataError("Price must be greater than zero");
+  }
 }

6392e8cf6fa91be1b6dacb25180afec96feb3a19
Leandro Gonzalez <leanlag@gmail.com>
2025-07-28 12:02:27 -0300

    feat(domain): agregar pruebas unitarias para el registro de usuario con manejo de errores

--- domain/src/use-cases/user/user-register.spec.ts
+++ domain/src/use-cases/user/user-register.spec.ts
@@ -8,6 +8,8 @@ import {
   UserRegisterDependencies,
   UserRegisterRequestModel,
 } from "./user-register";
+import { UserRole } from "../../entities/User";
+import { createInvalidDataError } from "../../errors/error";
 
 describe("User register use case", () => {
   let _mockedUserRepository: MockedUserRepository = mockUserRepository([]);
@@ -18,6 +20,56 @@ describe("User register use case", () => {
     _dependencies = {
       users: _mockedUserRepository,
     };
+
+    _mockedUserRepository.create({
+      id: "existing user id",
+      password: "12345678",
+      email: "existing@user.com",
+      name: "Existing User",
+      role: "user" as UserRole,
+    });
+  });
+
+  test("should fail if email is already in use", async () => {
+    const payload: UserRegisterRequestModel = {
+      email: "existing@user.com",
+      password: "12345678",
+      name: "Test User",
+    };
+    const result = await UserRegister(_dependencies, payload);
+    expect(result).toEqual(createInvalidDataError("Email already in use"));
+  });
+
+  test("should fail if email is empty", async () => {
+    const payload: UserRegisterRequestModel = {
+      email: "",
+      password: "12345678",
+      name: "Test User",
+    };
+    const result = await UserRegister(_dependencies, payload);
+    expect(result).toEqual(createInvalidDataError("Email must be not empty"));
+  });
+
+  test("should fail if password is empty", async () => {
+    const payload: UserRegisterRequestModel = {
+      email: "valid@email.com",
+      password: "",
+      name: "Test User",
+    };
+    const result = await UserRegister(_dependencies, payload);
+    expect(result).toEqual(
+      createInvalidDataError("Password must be not empty")
+    );
+  });
+
+  test("should fail if name is empty", async () => {
+    const payload: UserRegisterRequestModel = {
+      email: "valid@email.com",
+      password: "12345678",
+      name: "",
+    };
+    const result = await UserRegister(_dependencies, payload);
+    expect(result).toEqual(createInvalidDataError("Name must be not empty"));
   });
 
   test("should register user successfully with valid data", async () => {

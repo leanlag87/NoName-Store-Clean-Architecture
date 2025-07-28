aeaf2aaaeacbefa972b191253838edaa1f68ea33
Leandro Gonzalez <leanlag@gmail.com>
2025-07-28 10:53:12 -0300

    feat(domain): agregar manejo de errores en el registro de usuario

--- domain/src/use-cases/user/user-register.ts
+++ domain/src/use-cases/user/user-register.ts
@@ -1,7 +1,11 @@
 import { User } from "../../entities/User";
 import { UserRepository } from "../../repositories/user-repository";
+import { InvalidDataError, createInvalidDataError } from "../../errors/error";
 
-export type UserRegisterRequestModel = Omit<User, "id" | "role">;
+export type UserRegisterRequestModel = Omit<
+  User,
+  "id" | "role" | "createdAt" | "updatedAt"
+>;
 
 export interface UserRegisterDependencies {
   users: UserRepository;
@@ -9,17 +13,39 @@ export interface UserRegisterDependencies {
 
 export async function UserRegister(
   { users }: UserRegisterDependencies,
-  { email, password, name, createdAt, updatedAt }: UserRegisterRequestModel
-): Promise<User> {
+  { email, password, name }: UserRegisterRequestModel
+): Promise<InvalidDataError | User> {
+  const hasErrors = validateData(email, password, name);
+  if (hasErrors) return hasErrors;
+  
+  const existingUser = await users.findByEmail(email);
+  if (existingUser) return createInvalidDataError("Email already in use");
+
   const user: User = {
     id: crypto.randomUUID(),
     email,
     password,
     name,
     role: "user",
-    createdAt: createdAt || new Date(),
-    updatedAt: updatedAt || new Date(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
   };
 
   return await users.create(user);
+}
+
+function validateData(
+  email: string,
+  password: string,
+  name: string
+): InvalidDataError | void {
+  if (email.trim() === "") {
+    return createInvalidDataError("Email must be not empty");
+  }
+  if (password.trim() === "") {
+    return createInvalidDataError("Password must be not empty");
+  }
+  if (name.trim() === "") {
+    return createInvalidDataError("Name must be not empty");
+  }
 }

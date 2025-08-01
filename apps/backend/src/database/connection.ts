f12b717dc816ee8f902a1268650a0789c5a75e62
Leandro Gonzalez <leanlag@gmail.com>
2025-08-01 16:09:58 -0300

    feat(backend): agregar configuración de base de datos en env.ts

---
+++ apps/backend/src/database/connection.ts
@@ -0,0 +1,13 @@
+import { Dialect, Sequelize } from 'sequelize';
+import env from '../env';
+
+const {db: {dialect, host, name, password, port, username}} = env;
+
+let sequelizeConnection: Sequelize = new Sequelize(name, username, password, {
+  host,
+  dialect: dialect as Dialect,
+  port: +port,
+  logging: false
+});
+
+export default sequelizeConnection;
--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -11,3 +11,17 @@ export const DB_NAME = process.env.DB_NAME || 'noname_store';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '';
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const DB_USERNAME = process.env.DB_USERNAME || 'root';
+
+export default {
+    NODE_ENV,
+    PORT,
+    db: {
+      dialect: DB_DIALECT,
+      host: DB_HOST,
+      name: DB_NAME,
+      password: DB_PASSWORD,
+      port: DB_PORT,
+      username: DB_USERNAME
+    }
+  };
+

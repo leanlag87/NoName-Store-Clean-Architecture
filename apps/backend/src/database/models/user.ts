3b806c5c6e8e872a99498b1a43db676fa8830b3d
Leandro Gonzalez <leanlag@gmail.com>
2025-08-03 18:47:47 -0300

    feat(models): agregar modelo de usuario con atributos y configuración de Sequelize

---
+++ apps/backend/src/database/models/user.ts
@@ -0,0 +1,64 @@
+import { Model, DataTypes } from 'sequelize';
+import connection from '../connection';
+
+class User extends Model {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public password!: string;
+  public role?: 'admin' | 'user';
+  
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+  public readonly deletedAt?: Date;
+}
+
+User.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING(50),
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    role: {
+      allowNull: true,
+      type: DataTypes.ENUM('admin', 'user'),
+      defaultValue: 'user',
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    deletedAt: {
+      allowNull: true,
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize: connection,
+    modelName: 'User',
+    paranoid: true, 
+  }
+);
+
+export default User;

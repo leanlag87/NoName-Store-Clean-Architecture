759c492fdf4a75708e8c6d1bc8dd3ca89cd322c3
Leandro Gonzalez <leanlag@gmail.com>
2025-08-03 17:56:11 -0300

    feat(migrations): agregar migración para la tabla de carritos

---
+++ apps/backend/src/database/migrations/20250803202619-cart.ts
@@ -0,0 +1,46 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+
+/** @type {import('sequelize-cli').Migration} */
+
+export = {
+  async up(queryInterface: QueryInterface, Sequelize: typeof DataTypes) { 
+    await queryInterface.createTable('Carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+      totalAmount: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      }
+    });
+  },
+  async down(queryInterface: QueryInterface, Sequelize: any) {
+    await queryInterface.dropTable('Carts');
+  }
+};

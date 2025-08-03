7a78fd099f73290a9ef7a8162a4466c945a56dbd
Leandro Gonzalez <leanlag@gmail.com>
2025-08-03 19:25:41 -0300

    feat(models): agregar archivo de índice para exportar modelos

---
+++ apps/backend/src/database/models/index.ts
@@ -0,0 +1,14 @@
+import Cart from "./cart";
+import CartItem from "./cart-item";
+import Product from "./product";
+import User from "./user";
+
+const models = {
+    User,
+    Product,
+    Cart,
+    CartItem,
+};
+
+export default models;
+export { User, Product, Cart, CartItem };

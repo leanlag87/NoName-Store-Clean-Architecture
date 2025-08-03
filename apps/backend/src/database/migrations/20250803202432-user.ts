15bcc485307f1fb3219f646ca0c15d4dc8d6eebb
Leandro Gonzalez <leanlag@gmail.com>
2025-08-03 17:49:41 -0300

    feat(migrations): agregar migración para la tabla de usuarios

---
+++ apps/backend/src/database/migrations/20250803202432-user.ts
@@ -0,0 +1,49 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+
+/** @type {import('sequelize-cli').Migration} */
+
+export = {
+  async up(queryInterface: QueryInterface, Sequelize: typeof DataTypes) { 
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.ENUM('admin', 'user'),
+        allowNull: true,
+        defaultValue: 'user'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      }
+    });
+  },
+  async down(queryInterface: QueryInterface, Sequelize: any) {
+    await queryInterface.dropTable('Users');
+  }
+};

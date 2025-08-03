0b00eaa936a995a7761cc54bc9242780fa69a376
Leandro Gonzalez <leanlag@gmail.com>
2025-08-03 18:49:09 -0300

    feat(models): agregar modelo de producto con atributos y configuración de Sequelize

---
+++ apps/backend/src/database/models/product.ts
@@ -0,0 +1,67 @@
+import { Model, DataTypes } from 'sequelize';
+import connection from '../connection';
+
+class Product extends Model {
+  public id!: number;
+  public name!: string;
+  public description!: string;
+  public price!: number;
+  public categoryId?: number;
+  public brandId?: number;
+  
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+  public readonly deletedAt?: Date;
+}
+
+Product.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    description: {
+      allowNull: false,
+      type: DataTypes.TEXT,
+    },
+    price: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    categoryId: {
+      allowNull: true,
+      type: DataTypes.INTEGER,
+    },
+    brandId: {
+      allowNull: true,
+      type: DataTypes.INTEGER,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    deletedAt: {
+      allowNull: true,
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize: connection,
+    modelName: 'Product',
+    paranoid: true,
+  }
+);
+
+export default Product;

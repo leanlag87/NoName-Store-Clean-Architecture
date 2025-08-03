c28212239199035707f07c92940184d8c2cbc0df
Leandro Gonzalez <leanlag@gmail.com>
2025-08-03 18:33:28 -0300

    fix(migrations): corregir formato de comillas y agregar comas faltantes en la migración de artículos de carrito

--- apps/backend/src/database/migrations/20250803202633-cart-item.ts
+++ apps/backend/src/database/migrations/20250803202633-cart-item.ts
@@ -1,65 +1,65 @@
-import { QueryInterface, DataTypes } from 'sequelize';
+import { QueryInterface, DataTypes } from "sequelize";
 
 /** @type {import('sequelize-cli').Migration} */
 
 export = {
-  async up(queryInterface: QueryInterface, Sequelize: typeof DataTypes) { 
-    await queryInterface.createTable('CartItems', {
+  async up(queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    await queryInterface.createTable("CartItems", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       cartId: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Carts',
-          key: 'id'
+          model: "Carts",
+          key: "id",
         },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
       },
       productId: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Products',
-          key: 'id'
+          model: "Products",
+          key: "id",
         },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
       },
       quantity: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        defaultValue: 1
+        defaultValue: 1,
       },
       price: {
         type: Sequelize.INTEGER,
-        allowNull: false
+        allowNull: false,
       },
       addedAt: {
         type: Sequelize.DATE,
         allowNull: false,
-        defaultValue: Sequelize.NOW
+        defaultValue: Sequelize.NOW,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       deletedAt: {
         allowNull: true,
         type: Sequelize.DATE,
-      }
+      },
     });
   },
-  async down(queryInterface: QueryInterface, Sequelize: any) {
-    await queryInterface.dropTable('CartItems');
-  }
+  async down(queryInterface: QueryInterface) {
+    await queryInterface.dropTable("CartItems");
+  },
 };

d85cde663dbd202a518d205a9e8a1d616f1f7bfd
Leandro Gonzalez <leanlag@gmail.com>
2025-08-16 15:26:44 -0300

    feat(routes): reestructurar rutas de usuario y simplificar la importación del controlador

--- apps/backend/src/routes/user.routes.ts
+++ apps/backend/src/routes/user.routes.ts
@@ -1,9 +1,10 @@
+import { Router } from "express";
 import { userController } from "src/controllers/user.controller";
 
-const controller = userController();
-router.post("/register", controller.registerNewUser);
-router.get("/profile", authMiddleware, controller.getCurrentUser);
-router.get("/:id", controller.getUserById);
-router.get("/", controller.getAllUsers);
-router.put("/:id", authMiddleware, controller.updateUser);
-router.delete("/:id", authMiddleware, controller.deleteUser);
+const router = Router();
+
+const { registerNewUser } = userController();
+
+router.post("/register", registerNewUser);
+
+export default router;
